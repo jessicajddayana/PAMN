c7efbad541dfa1c0e1c93cc8d54f6e74b1a7c52a
Jéssica Bustamante <jessica-bustamante@outlook.com>
2016-04-15 23:53:33 -0500

    Create Bebida.java
    Producto de Tienda

---
+++ Bebida.java
@@ -0,0 +1,16 @@
+public class Bebida  //se declara la clase bebida, un producto de la Tiendita
+{
+	 String nombre;   //se declara para dar un nombre al producto 	
+	 int precio;		//se declara para asignarle el precio al producto
+	 	//se declara el producto y se le asignan tanto nombre como precio
+	public Bebida()
+		{
+			nombre = "agua";
+			precio = 9;
+		}
+	public Bebida(String nom,int p)
+		{
+	 	nombre = nom;
+	 	precio = p;
+		}
+}
