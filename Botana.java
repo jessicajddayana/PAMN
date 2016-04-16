34fda4f6e1feb2d975c0ea10ac32de994b025369
Jéssica Bustamante <jessica-bustamante@outlook.com>
2016-04-15 23:53:06 -0500

    Create Botana.java
    Producto de Tienda

---
+++ Botana.java
@@ -0,0 +1,16 @@
+public class Botana //se declara la clase botana, un producto de la tienda
+{
+	 String nombre; //se declara para asignar un nombre
+	 int precio;    //se declara para asignar un precio
+	 //se le ingresan datos a botana 
+	public Botana()  
+		{
+			nombre = "papas";
+			precio = 8;
+		}
+	public Botana(String nom,int p)
+		{
+	 	nombre = nom;
+	 	precio = p;
+		}
+}
