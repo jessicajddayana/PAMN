50793fe2ae051361caa90e68691c87d64cd1a42d
Jéssica Bustamante <jessica-bustamante@outlook.com>
2016-04-15 23:52:29 -0500

    Create Comida.java
    Producto de Tienda

---
+++ Comida.java
@@ -0,0 +1,17 @@
+public class Comida //se declara la clase comida, un producto de la tiendita
+{
+	 String nombre; //se declara para dar un nombre al producto 	
+	 int precio;	//se declara para asignarle el precio al producto
+	 	//se declara el producto y se le asignan tanto nombre como precio
+	 
+	public Comida()
+		{
+			nombre = "queso";
+			precio = 8;
+		}
+	public Comida(String nom,int p)
+		{
+	 	nombre = nom;
+	 	precio = p;
+		}
+}
